0820c250538a8fdda5c9e452a9f97d4609f52fce
haydn <haydnt@zohomail.com>
2022-08-13 09:07:44 -0700

    added a declaration for a void function that will make the color pallet

--- src/colors.h
+++ src/colors.h
@@ -3,12 +3,13 @@
 #define COLOR_FUNCTIONS
 
 #include "png++/png.hpp"
-
+#include <string>
 
 namespace pallet {
 
   png::image< png::index_pixel > getPixel (int width, int height);
-
+  
+  void makeColorPallet(std::string path, int size);
 
 }
 
