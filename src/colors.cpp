2635aeba19bb44946fd05f09e2d451987aece24e
haydn <haydnt@zohomail.com>
2022-08-13 09:08:08 -0700

    added the color pallet function to be implemented later

---
+++ src/colors.cpp
@@ -0,0 +1,19 @@
+#include "colors.h"
+#include "png++/png.hpp"
+#include <string>
+
+
+
+namespace pallet
+{
+ 	//png::image< png::index_pixel > getPixel (int width, int height){}
+ 
+	void makeColorPallet(std::string path, int size)
+	{	
+	
+	
+	}
+
+
+}
+
