b6aab808941a03a0f1c138a86c12f43c4d9b8bb0
haydn <haydnt@zohomail.com>
2022-08-12 19:14:32 -0700

    getting color fetching stuff ready

---
+++ src/colors.h
@@ -0,0 +1,15 @@
+
+#ifndef COLOR_FUNCTIONS
+#define COLOR_FUNCTIONS
+
+#include "png++/png.hpp"
+
+
+namespace pallet {
+
+  png::image< png::index_pixel > getPixel (int width, int height);
+
+
+}
+
+#endif
