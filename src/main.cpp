72d90e6b521aec323454cacd6f1588f3a387317b
haydn <haydnt@zohomail.com>
2022-08-12 19:14:08 -0700

    main function has CMD args

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <string>
+#include "png++/png.hpp"
 
 
-
-int main()
+int main(int argc, char** argv)
 {
+	
+	
 
-	std::cout << "he";
+	std::string path {argv[1]};//this is our path to the image
+
+	if (path == "--help")
+	{
+	
+		std::cout << "test";
+		return 0;
+
+	}
+
+
+
+	std::string  palletSizeInput {argv[2]};//and this is the size of the color pallet
+
+	int palletSize {std::stoi(palletSizeInput)};	
+
+
+	std::cout << "Generating a " << palletSize<<" color pallet from " << path << "..." << '\n';		
 	return 0;
 
 }
