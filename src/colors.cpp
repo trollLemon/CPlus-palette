212e3de46f202c0e5de786dae0adf60765e57d1b
haydn <haydnt@zohomail.com>
2022-08-14 11:46:48 -0700

    finished funciton to get the colors of the pixels in the image

--- src/colors.cpp
+++ src/colors.cpp
@@ -1,7 +1,7 @@
 #include "colors.h"
 #include "png++/png.hpp"
 #include <string>
-
+#include <map>
 
 
 namespace pallet
@@ -9,17 +9,22 @@ namespace pallet
 
 	//return the pixel at the given indexes
  	 png::rgb_pixel  getPixel ( png::image< png::rgb_pixel > image, int width, int height){
-	 
-		 return image[width][height];
+		 return image[height][width];
 	 
 	 }
 	
 
 	//convert rgb values to hexidecimal
 	// code snippet by Nikos C from this stackoverflow post: https://stackoverflow.com/questions/14375156/how-to-convert-a-rgb-color-value-to-an-hexadecimal-value-in-c 
-	unsigned long createRGB(int r, int g, int b)
+	 std::string createHex(int r, int g, int b)
 	{
-	 return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+	
+		std::stringstream hex;
+
+		hex << "#";
+		hex << std::hex << (r << 16 | g << 8 | b);
+		return hex.str();	
+	
 	}
 
 	
@@ -31,24 +36,43 @@ namespace pallet
 	 * the amount of colors dictated by the size variable.
 	 */ 
 	void makeColorPallet(std::string path, int size)
-	{	
-	
-	png::image< png::rgb_pixel > image(path);//read the image
-
-	uint height {image.get_height()};
-	uint width {image.get_width()};
-
-	for (uint h; h < height; ++h)
 	{
-		for(uint w; w < width; ++w )
-		{
+	
+
+	std::map<std::string, long> colors;
+	
 		
-		}
+	png::image< png::rgb_pixel > image(path);//read the image
+        uint height {image.get_height()};
+	uint width {image.get_width()};	
+	uint increment = 100; //this is how many pixels we incrememnt over	
+	
+	for(uint i = 0; i< height; i = i + increment)
+	{
+		
+		
+		for(uint j = 0; j < width; j= j + increment)
+		{
+			png::rgb_pixel pixel {getPixel(image, j, i)};
+
+			std::string hex { createHex(pixel.red, pixel.green, pixel.blue)};
+			
+			//map stuff
+			if (!colors.count(hex))
+			{
+				colors.insert({hex, 1});
+			} 
+
+			else 
+			{
+			colors[hex]++;
+			
+			}
+		}	
 	}
-
-
+	
+		
 	}
-
 
 }
 
