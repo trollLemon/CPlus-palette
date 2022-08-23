f5be7ef6923cbd5cc1c9689541e9e7956f9b3b4e
trolllemon <haydnt@zohomail.com>
2022-08-22 21:34:36 -0700

    getting ready for the k mean cluster code

---
+++ src/kmean.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <array>
+#include "kmean.h"
+namespace kmean_cluster {
+
+
+	
+
+
+}
+
+
+
---
+++ src/kmean.h
@@ -0,0 +1,41 @@
+
+#ifndef PALETTEGEN
+#define PALETTEGEN
+
+#include <vector>
+#include <array>
+namespace kmean_cluster {
+static double maxDiff = 20.0;
+ 
+	struct Point {
+
+		unsigned char r;
+		unsigned char g;
+		unsigned char b;
+
+	};
+
+	struct Cluster {
+
+		std::vector<Point> points;
+
+	};
+
+
+	std::vector<Point> makePointsFromImageData (std::vector<std::array<unsigned char, 3>> colorData);
+	
+	Point makePoint (std::array<unsigned char, 3 > rgb);
+
+	Cluster makeCluster (Point randomPoint);
+	
+	std::vector<Point> makePalette (std::vector<std::array<unsigned char ,3>> colorData);
+	
+	double distance(Point p, Point q);
+	
+  Cluster assign(Cluster cluster, std::vector<Point> points);
+	
+	Point calculateCentriod(Cluster cluster);
+
+}
+
+#endif
