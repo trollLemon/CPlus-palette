a0ffef37101bcfa4006aef75b26f4c87d3ffa76d
haydn <haydnt@zohomail.com>
2022-10-10 16:48:42 -0700

    class method definitions, along with moving the centroid calc code into a cluster method

--- src/dataTypes.cpp
+++ src/dataTypes.cpp
@@ -1,50 +1,86 @@
 #include "dataTypes.h"
 #include <vector>
-Point::Point(int red, int green, int blue)
-		{
-			this->r = red;
-			this->g = green;
-			this->b = blue;
-			this->id = -1; //-1 means the point hasnt been put in a cluster yet
-		};
-Point::Point()
-		{
-		this->r =0;
-		this->g =0;
-		this ->b =0;
-		this->id =0;
+#include <array>
+#include <cmath>
 
-		};
+//=======Point Class===========
 
-		int Point::sumRGB()
-		{
-			return this->r+this->g+this->b;
-		};
-Cluster::Cluster(Point p, int id)
-	{
-		this-> centroid =p;
-		this-> id = id;
-		this-> data.push_back(p);
-	};
+Point::Point(int r, int g, int b)
+{
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    id = -1;
+}
 
-	void Cluster::addPoint(Point p){
-		this->data.push_back(p);
-	};
-	void Cluster::setCentroid(Point p){
-		
-		this->centroid = p;
-		
-		};
-	std::vector<Point>& Cluster::getData(){
-		return this->data;
-	};
-	void Cluster::resetCentroid(){
-		this->data.clear();
-	};
+Point::Point(){}
 
-		
-
-
-
+int Point::getId()
+{
+    return id;
+}
 	
+void Point::setId(int n)
+{
+    id = n;
+}
+
+std::vector<int> Point::getRGB()
+{ 
+    return {r,g,b};
+}
+
+int Point::sumRGB()
+{
+    return r+g+b;
+}
+
+//====Cluster Class======
+
+Cluster::Cluster(Point& p, int id)
+{
+    centroid = p;
+    this->id = id;
+}
+
+int Cluster::getId()
+{
+    return id;
+}
+
+void Cluster::calculateNewCentroid()
+{
+
+    std::array<int,3> averages ={0,0,0};
+    int size{0};
+
+    for(uint p{0}; p < data.size(); ++p)
+	{		
+        std::vector<int> colorData {data[p].getRGB()};
+                
+        int r = colorData[0];
+        int g = colorData[1];
+        int b = colorData[2];
+        averages[0] += r*r;
+		averages[1] += g*g;
+		averages[2] += b*b;
+		++size;	
+	}
+	this->reset();
+    Point newCentroid = Point(std::sqrt(averages[0]/size), std::sqrt(averages[1]/size), std::sqrt(averages[2]/size));
+    this->centroid = newCentroid;
+    addPoint(newCentroid);
+}
+
+
+
+std::vector<Point>& Cluster::getData()
+{
+    return data;
+}
+
+void Cluster::reset()
+{
+    data.clear();
+}
 
