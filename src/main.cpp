69bd51a89b96b1c30836027482c827cffb9db71a
haydn <haydnt@zohomail.com>
2022-10-16 10:02:18 -0700

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,45 @@
+#include "CImg.h"
+#include "colors.h"
 #include <iostream>
 #include <string>
-#include "colors.h"
-#include "CImg.h"
 
-void printHelp(std::string programName)
-{
-		std::cout << "Usage:\n " << programName << ": pathToImage numberOfColors\n";
-        std::cout << "Example: " << programName << " ~/Pictures/picture.png 8\n";
-
+void printHelp(std::string programName) {
+    std::cout << "Usage:\n " << programName << ": pathToImage numberOfColors\n";
+    std::cout << "Example: " << programName << " ~/Pictures/picture.png 8\n";
 }
 
-int main(int argc, char** argv)
-{
-    if (argc != 3){
+int main(int argc, char **argv) {
+    if (argc != 3) {
         printHelp(argv[0]);
-        return 1;        
-    }   
-	
-    std::string path {argv[1]};//this is our path to the image
-
-	  if (path == "--help")
-	  {
-        printHelp(argv[0]);	
-		    return 0;
-
-	  }
-
-
-
-	  std::string  paletteSizeInput {argv[2]};//and this is the size of the colo:w
-
-	  int paletteSize {std::stoi(paletteSizeInput)};	
-
-
-	  std::cout << "Generating a " << paletteSize<<" color palette from " << path << "..." << '\n';		
-	
-    //take in user inputs and create a color palette, and return an Enum telling us if it 
-    //was successful or not
-    
-    try
-    {
-    palette::makeColorPalette(path, paletteSize);
+        return 1;
     }
-    catch(cimg_library::CImgIOException)
-    {
+
+    std::string path{argv[1]}; // this is our path to the image
+
+    if (path == "--help") {
+        printHelp(argv[0]);
+        return 0;
+    }
+
+    std::string paletteSizeInput{argv[2]}; // and this is the size of the colo:w
+
+    int paletteSize{std::stoi(paletteSizeInput)};
+
+    std::cout << "Generating a " << paletteSize << " color palette from "
+              << path << "..." << '\n';
+
+    // take in user inputs and create a color palette, and return an Enum
+    // telling us if it was successful or not
+
+    try {
+        palette::makeColorPalette(path, paletteSize);
+    } catch (cimg_library::CImgIOException) {
         std::cout << "Failed to load " << path << '\n';
         return 1;
-    }
-    catch(cimg_library::CImgArgumentException)
-    {
-        std::cout << "Failed to load " << path <<", it is a directory" << '\n'; 
+    } catch (cimg_library::CImgArgumentException) {
+        std::cout << "Failed to load " << path << ", it is a directory" << '\n';
         return 1;
     }
-	    return 0;
-
+    return 0;
 }
-
-
 
