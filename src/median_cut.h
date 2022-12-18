96c4ef94bdbdfd87b9a6e7963eedf93e54f48a0a
haydn <haydnt@zohomail.com>
2022-12-17 20:51:55 -0800

    removed comments

--- src/median_cut.h
+++ src/median_cut.h
@@ -1,12 +1,5 @@
 
 
-//    Move all pixels into a single large bucket.
-//    Find the color channel (red, green, or blue) in the image with the greatest range.
-//    Sort the pixels by that channel values.
-//    Find the median and cut the region by that pixel.
-//    Repeat the process for both buckets until you have the desired number of colors.
-
-
 #ifndef MEDIANCUT
 #define MEDIANCUT
 #include <vector>
