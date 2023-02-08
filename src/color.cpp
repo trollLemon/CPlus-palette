46379414ca84e8f1041ffdad9310424087792b5c
haydn <haydnt@zohomail.com>
2023-02-08 15:36:58 -0800

    added functions for getting values

--- src/color.cpp
+++ src/color.cpp
@@ -1,22 +1,20 @@
 #include "color.h"
 
-Color::Color(int r, int g, int b) : r{r}, g{g}, b{b} {}
+Color::Color(int r, int g, int b, int p) : r{r}, g{g}, b{b}, p{p} {}
 std::string Color::asHex() {
 
   char hex[8];
-    std::snprintf(hex, sizeof hex, "#%02x%02x%02x", r, g, b);
+  std::snprintf(hex, sizeof hex, "#%02x%02x%02x", r, g, b);
 
-    std::string hexString;
+  std::string hexString;
 
-    for (char i : hex) {
-        hexString += i;
-    }
+  for (char i : hex) {
+    hexString += i;
+  }
 
-    return hexString;
-
+  return hexString;
 }
 
-int Color::Red() {return r;}
-int Color::Green() {return g;}
-int Color::Blue() {return b;}
-
+int Color::Red() { return r; }
+int Color::Green() { return g; }
+int Color::Blue() { return b; }
