7fcef0985b813014ab5b479c3439ae85cce10e44
haydn <haydnt@zohomail.com>
2023-02-24 15:54:25 -0800

    custom sort

--- src/quantizer.h
+++ src/quantizer.h
@@ -5,8 +5,8 @@
 #include <algorithm>
 #include <map>
 #include <queue>
-#include <string>
 #include <set>
+#include <string>
 #include <unordered_map>
 
 // Cluster id and the distance from a point to its centroid
@@ -14,23 +14,30 @@ struct cluster_distance {
 
   int cluster;
   double distance;
+  ~cluster_distance();
+};
 
+
+
+struct ColorSort {
+
+  bool operator()(const Cluster *a, const Cluster *b);
 };
 
 struct Comp {
 
-bool operator()(const cluster_distance *a, const cluster_distance *b);
+  bool operator()(const cluster_distance *a, const cluster_distance *b);
 };
-
-
 
 // wrapper around a priority queue for a Min heap
 class minHeap {
 
 private:
-  std::priority_queue<cluster_distance *, std::vector<cluster_distance *>, Comp> distances;
+  std::priority_queue<cluster_distance *, std::vector<cluster_distance *>, Comp>
+      distances;
 
 public:
+  ~minHeap();
   void push(cluster_distance *pair);
   int pop();
   void clear();
@@ -41,13 +48,12 @@ class Quantizer {
 
 private:
   std::map<Color *, minHeap *> data; // Distances from points to each centroid
-  std::set<Cluster *>
-      toReclalculate; // Clusters whose centroid we need to recalculate
   std::unordered_map<int /*Cluster ID*/, Cluster *> clusters; // clusters
   void K_MEAN_INIT(int k); // select k random points to be the initial Clusters
   void K_MEAN_START();     // begins the data clustering
   double EuclidianDistance(Color *a, Color *b);
   std::vector<Color *> colors;
+  void reCalculateCentroids(std::set<Cluster *>);
 
 public:
   // returns a vector of strings for each color in the palette.
