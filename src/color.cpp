e1037e96b098a884ad314dce6b4f04ab768325e4
haydn <haydnt@zohomail.com>
2023-02-24 11:24:32 -0800

    LABtoRGB

--- src/color.cpp
+++ src/color.cpp
@@ -1,11 +1,24 @@
 #include "color.h"
 #include <cmath>
 #include <iostream>
-Color::Color(int r, int g, int b, int p) : r{r}, g{g}, b{b}, p{p} {
+Color::Color(int r, int g, int b, int p)
+    : r{r}, g{g}, b{b}, p{p}, clusterId(-1) {
 
   // convert to XYZ and LAB on color creation
   RGBtoLAB();
 }
+
+
+Color::Color(double lum, double aVal, double bVal, int p)
+    : L{lum}, A{aVal}, B{bVal}, p{p}, clusterId(-1) {
+
+  // convert to XYZ and RGB on color creation
+  LABtoRGB();
+}
+
+
+
+
 std::string Color::asHex() {
 
   char hex[8];
@@ -65,27 +78,82 @@ void Color::XYZtoLAB() {
     ratioX = (7.787 * ratioX) + (16.0 / 116.0);
   }
 
-if (ratioY > 0.008856) {
+  if (ratioY > 0.008856) {
     ratioY = std::pow(ratioY, (1.0 / 3.0));
   } else {
     ratioY = (7.787 * ratioY) + (16.0 / 116.0);
   }
 
-if (ratioZ > 0.008856) {
+  if (ratioZ > 0.008856) {
     ratioZ = std::pow(ratioZ, (1.0 / 3.0));
   } else {
     ratioZ = (7.787 * ratioZ) + (16.0 / 116.0);
   }
 
-
-
-L = (116.0 * ratioY) - 16.0;
-A = 500.0 * (ratioX-ratioY);
-B = 200.0 * (ratioY-ratioZ);
-
+  L = (116.0 * ratioY) - 16.0;
+  A = 500.0 * (ratioX - ratioY);
+  B = 200.0 * (ratioY - ratioZ);
 }
-void Color::LABtoXYZ() {}
-void Color::XYZtoRGB() {}
+void Color::LABtoXYZ() {
+
+  double ratioY = (L + 16.0) / 116.0;
+  double ratioX = A / 500.0 + ratioY;
+  double ratioZ = ratioY - B / 200.0;
+
+  if (std::pow(ratioY, 3.0) > 0.008856) {
+    ratioY = std::pow(ratioY, 3.0);
+  } else {
+    ratioY = (ratioY - 16.0 / 116.0) / 7.787;
+  }
+
+  if (std::pow(ratioX, 3.0) > 0.008856) {
+    ratioX = std::pow(ratioX, 3.0);
+  } else {
+    ratioX = (ratioX - 16.0 / 116.0) / 7.787;
+  }
+
+  if (std::pow(ratioZ, 3.0) > 0.008856) {
+    ratioZ = std::pow(ratioZ, 3.0);
+  } else {
+    ratioZ = (ratioZ - 16.0 / 116.0) / 7.787;
+  }
+
+  x = ratioX * X_2;
+  y = ratioY * Y_2;
+  z = ratioZ * Z_2;
+}
+void Color::XYZtoRGB() {
+
+  double ratioX = x / 100.0;
+  double ratioY = y / 100.0;
+  double ratioZ = z / 100.0;
+
+  double ratioR = ratioX * 3.2406 + ratioY * -1.5372 + ratioZ * -0.4986;
+  double ratioG = ratioX * -0.9689 + ratioY * 1.8758 + ratioZ * 0.0415;
+  double ratioB = ratioX * 0.0557 + ratioY * -0.2040 + ratioZ * 1.0570;
+
+  if (ratioR > 0.0031308) {
+    ratioR = 1.055 * std::pow(ratioR, (1.0 / 2.4)) - 0.055;
+  } else {
+    ratioR = 12.92 * ratioR;
+  }
+
+  if (ratioG > 0.0031308) {
+    ratioG = 1.055 * std::pow(ratioG, (1.0 / 2.4)) - 0.055;
+  } else {
+    ratioG = 12.92 * ratioG;
+  }
+
+  if (ratioB > 0.0031308) {
+    ratioB = 1.055 * std::pow(ratioB, (1.0 / 2.4)) - 0.055;
+  } else {
+    ratioB = 12.92 * ratioB;
+  }
+
+  r = ratioR * 255;
+  g = ratioG * 255;
+  b = ratioB * 255;
+}
 
 // Convert RGB to LAB
 void Color::RGBtoLAB() {
@@ -95,17 +163,22 @@ void Color::RGBtoLAB() {
 }
 // converts LAB to RGB
 // Assumes that RGB was already converted to LAB
-void Color::LABtoRGB() {}
+void Color::LABtoRGB() {
+
+  LABtoXYZ();
+  XYZtoRGB();
+}
 void Color::testColor() {
 
   std::cout << "Color:" << p << std::endl;
 
-  std::cout << "RGB: " << r << " " << g << " " << b << std::endl;
+  std::cout << "RGB: " << r << " " << g << " " << b << std::endl;	
+  std::cout << "XYZ: " << x << " " << y << " " << z << std::endl;
   std::cout << "LAB: " << L << " " << A << " " << B << std::endl;
 }
 int Color::getId() { return p; }
-int Color::getClusterId() { return ClusterId; }
-void Color::setClusterId(int i) { ClusterId = i; }
+int Color::getClusterId() { return clusterId; }
+void Color::setClusterId(int i) { clusterId = i; }
 int Color::Red() { return r; }
 int Color::Green() { return g; }
 int Color::Blue() { return b; }
