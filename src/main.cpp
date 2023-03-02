c2943a8b2bb75d40661b514cf9b96b5a51faf5eb
haydn <haydnt@zohomail.com>
2023-03-02 11:09:17 -0800

    added option

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,21 @@
 #include <iostream>
 #include <string>
 void printHelp(std::string programName) {
-  std::cout << "Usage:\n " << programName << ": pathToImage numberOfColors\n";
-  std::cout << "Example: " << programName << " ~/Pictures/picture.png 8\n";
+  std::cout << "Usage:\n " << programName
+            << ": pathToImage numberOfColors -t [quantization type]"
+            << std::endl;
+  std::cout << "Example: " << programName << " ~/Pictures/picture.png 8 -t 1\n"
+            << std::endl;
+  std::cout << "-t 1: uses K mean Clustering for Color Palette Generation: "
+               "slower but produces better palettes most of the time"
+            << std::endl;
+  std::cout << "-t 2: used Median Cut for Color Palette Generation: Faster "
+               "than K mean Clustering but color palettes aren't always as good"
+            << std::endl;
 }
 
 int main(int argc, char **argv) {
-  if (argc != 3) {
+  if (argc < 3) {
     printHelp(argv[0]);
     return 1;
   }
@@ -20,23 +29,38 @@ int main(int argc, char **argv) {
     return 0;
   }
 
-  std::string paletteSizeInput{argv[2]}; // and this is the size of the colo:w
+  std::string paletteSizeInput{argv[2]}; // and this is the size of the color
 
   int paletteSize{std::stoi(paletteSizeInput)};
 
-  if (paletteSize < 0) {
-    std::cout << "Cannot make a palette with size less that 0" << std::endl;
+  if (paletteSize <= 0) {
+    std::cout << "Cannot make a palette with size less or equal to 0"
+              << std::endl;
     return 0;
   }
 
   std::cout << "Generating a " << paletteSize << " color palette from " << path
             << "..." << '\n';
 
+  int genType = 1;
+
+  if(argc > 4){
+  if (argv[4]) {
+    genType = *argv[4] - '0';
+  }
+
+
+  if (genType < -1 || genType > 2) {
+    printHelp(argv[0]);
+    return 1;
+  }
+
+  }
   // take in user inputs and create a color palette, and return an Enum
   // telling us if it was successful or not
 
   try {
-    palette::makeColorPalette(path, paletteSize);
+    palette::makeColorPalette(path, paletteSize, genType);
   } catch (cimg_library::CImgIOException) {
     std::cout << "Failed to load " << path << '\n';
     return 1;
