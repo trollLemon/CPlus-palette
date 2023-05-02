68fc9d98b7fcb9a438522384a899b94e9dc3cc23
Haydn <haydnt@zohomail.com>
2023-05-02 15:50:32 -0700

    docs

---
+++ HEADERS/median_cut_helpers.h
@@ -0,0 +1,31 @@
+/* *
+ * median_cut_helpers.h
+ *
+ * This file contains declarations for 
+ * helper funcitons for median cut.
+ *
+ *
+ *
+ * */
+
+#ifndef HELPERS
+#define HELPERS
+
+#include <cmath>
+
+/* *
+ * Returns true if the inputed number is a power of 2.
+ *
+ * This function will be used by the client code to determine 
+ * if the user-inputed size of the palette is a power of two.
+ *
+ * If the size isn't a power of two, we need to round up to 
+ * the next power of two since medain cut only works with powers of 2
+ * */
+bool isPowerOfTwo(int x);
+
+/* *
+ * Returns the number rounded up to the next power of two
+ * */
+int powerOfTwoSize(int size);
+#endif
