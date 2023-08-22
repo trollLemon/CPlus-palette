2c1f4642ad6b58ccd1cb219c3d18e756e368540b
haydn <trolllemon@protonmail.com>
2023-08-22 14:54:29 -0700

    moved into the cuda dir

--- src/kmean/k_mean.cu
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-// CUDA kernel to add two arrays on the GPU
-__global__ void addArrays(int* a, int* b, int* c, int size) {
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid < size) {
-        c[tid] = a[tid] + b[tid];
-    }
-}
