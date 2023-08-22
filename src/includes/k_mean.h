0a95acae27b0e2e73d4f599a0de99fafa5270d85
haydn <trolllemon@protonmail.com>
2023-08-22 14:52:10 -0700

    moved declarations to util.h

--- src/includes/k_mean.h
+++ src/includes/k_mean.h
@@ -21,42 +21,7 @@
 #include <set>
 #include <string>
 #include <unordered_map>
-
-// Cluster id and the distance from a point to its centroid
-
-struct cluster_distance {
-
-  int cluster;
-  double distance;
-  ~cluster_distance();
-};
-
-// custom comparator to sort clusters
-struct ColorSort {
-
-  bool operator()(const Cluster *a, const Cluster *b);
-};
-
-// custom comparator to compare cluster_distances
-struct Comp {
-
-  bool operator()(const cluster_distance *a, const cluster_distance *b);
-};
-
-// wrapper around a priority queue for a Min heap
-class minHeap {
-
-private:
-  std::priority_queue<cluster_distance *, std::vector<cluster_distance *>, Comp>
-      distances;
-
-public:
-  ~minHeap();
-  void push(cluster_distance *pair);
-  int pop();
-  void clear();
-};
-
+#include "util.h"
 class KMean {
 
 private:
