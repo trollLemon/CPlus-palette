200b972dfb90db9fa0dba523a303c214eff01af8
haydn <trolllemon@protonmail.com>
2023-08-25 11:33:33 -0700

    moved CPU loading to CPU code areas

--- src/kmean/cuda/k_mean.cu
+++ src/kmean/cuda/k_mean.cu
@@ -11,12 +11,9 @@
 #include <iostream>
 #include <random>
 #include <unordered_set>
+#include "util.h"
 
-struct CUDA_COLOR_DATA {
-  int r;
-  int g;
-  int b;
-};
+typedef pixel CUDA_COLOR_DATA;
 
 __device__ double euclideanDistance(CUDA_COLOR_DATA x, CUDA_COLOR_DATA y) {
   double dl = static_cast<double>(y.r) - static_cast<double>(x.r);
@@ -29,7 +26,8 @@ __global__ void recalcClusters(CUDA_COLOR_DATA *d_clusters,
                                CUDA_COLOR_DATA *d_colors, int *assignments,
                                int color_count, int *clusterStats) {
 
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	  int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < color_count) {
 
     int clusterId = assignments[idx];
@@ -46,34 +44,39 @@ __global__ void recalcClusters(CUDA_COLOR_DATA *d_clusters,
       d_clusters[clusterId].b /= numPoints;
     }
   }
+
 }
 
 __global__ void assignPoints(CUDA_COLOR_DATA *d_clusters,
                              CUDA_COLOR_DATA *d_colors, int *assignments, int k,
                              int color_count, int *cluster_stats) {
 
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx < color_count) {
+	const int idx = blockIdx.x*blockDim.x + threadIdx.x;
 
-    double distance = INFINITY;
-    int cluster_idx = -1;
+	if(idx>color_count) return;
 
-    for (int i = 0; i < k; ++i) {
+	double min_dist = INFINITY;
+	int closest_centroid = 0;
 
-      double dist = euclideanDistance(d_clusters[i], d_colors[idx]);
-      if (dist < distance) {
-        cluster_idx = i;
-        distance = dist;
-      }
-    }
 
-    assignments[idx] = cluster_idx;
-    atomicAdd(&cluster_stats[cluster_idx], 1);
-  }
+	for(int i = 0; i<k; ++i){
+	 double dist = euclideanDistance(d_clusters[i],d_colors[idx]);
+
+	 if (dist<min_dist){
+		min_dist =dist;
+		closest_centroid =i;
+	 }
+
+	}
+
+	assignments[idx]=closest_centroid;
+   atomicAdd(&cluster_stats[closest_centroid], 1);
 }
 
 __global__ void initClusters(CUDA_COLOR_DATA *d_clusters,
                              CUDA_COLOR_DATA *d_colors, int size, int seed) {
+
+
 
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -83,37 +86,12 @@ __global__ void initClusters(CUDA_COLOR_DATA *d_clusters,
     int point_idx = curand(&state) % (size - 1);
     d_clusters[idx] = d_colors[point_idx];
   }
+
+
 }
 
 std::vector<std::string>
-CudaKmeanWrapper(cimg_library::CImg<unsigned char> *image, int size) {
-  int height{image->height()};
-  int width{image->width()};
-
-  std::unordered_set<std::string> seen;
-  CUDA_COLOR_DATA colors[height * width];
-  ADV_Color *base = new ADV_Color(0, 0, 0);
-  int totalPixels = 0;
-  for (int i = 0; i < height; ++i) {
-    for (int j = 0; j < width; ++j) {
-      int r = *image->data(j, i, 0, 0);
-      int g = *image->data(j, i, 0, 1);
-      int b = *image->data(j, i, 0, 2);
-
-      base->setRGB(r, g, b);
-      std::string hex = base->asHex();
-
-      if (seen.count(hex) == 0) {
-        CUDA_COLOR_DATA color;
-        color.r = r;
-        color.g = g;
-        color.b = b;
-        colors[totalPixels] = color;
-        seen.insert(hex);
-        totalPixels++;
-      }
-    }
-  }
+CudaKmeanWrapper(CUDA_COLOR_DATA* pixel_data, int size, int totalPixels) {
 
   CUDA_COLOR_DATA *d_colors;
   CUDA_COLOR_DATA *d_clusters;
@@ -129,11 +107,11 @@ CudaKmeanWrapper(cimg_library::CImg<unsigned char> *image, int size) {
                  sizeof(int)); // points are not given a cluster yet (id is -1)
  
   cudaMemset(d_cluster_stats, 1, size * sizeof(int));
-  cudaMemcpy(d_colors, colors, totalPixels * sizeof(CUDA_COLOR_DATA),
+  cudaMemcpy(d_colors, pixel_data, totalPixels * sizeof(CUDA_COLOR_DATA),
              cudaMemcpyHostToDevice);
 
   bool loop = true;
-  int threadsPerBlock = 256;
+  int threadsPerBlock = 32;
   int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
   std::random_device rd;
   int seed = rd();
@@ -141,20 +119,20 @@ CudaKmeanWrapper(cimg_library::CImg<unsigned char> *image, int size) {
                                                    seed);
   cudaDeviceSynchronize();
  int x =0;
-  while(x++!=100){
+  while(x++!=5){
 
   blocksPerGrid = (totalPixels + threadsPerBlock - 1) / threadsPerBlock;
   assignPoints<<<blocksPerGrid, threadsPerBlock>>>(
       d_clusters, d_colors, d_assignments, size, totalPixels, d_cluster_stats);
-  cudaDeviceSynchronize();
   recalcClusters<<<blocksPerGrid, threadsPerBlock>>>(
-      d_clusters, d_colors, d_assignments, totalPixels, d_cluster_stats);
+    d_clusters, d_colors, d_assignments, totalPixels, d_cluster_stats);
 
+  cudaDeviceSynchronize();
 
   cudaMemset(d_cluster_stats, 1, size * sizeof(int));
 
-  }
   //}
+  }
 
   CUDA_COLOR_DATA *h_colors =
       (CUDA_COLOR_DATA *)malloc(size * sizeof(CUDA_COLOR_DATA));
@@ -162,8 +140,8 @@ CudaKmeanWrapper(cimg_library::CImg<unsigned char> *image, int size) {
   cudaMemcpy(h_colors, d_clusters, size * sizeof(CUDA_COLOR_DATA),
              cudaMemcpyDeviceToHost);
 
+
   ADV_Color color_helper(0,0,0);
-	
   std::vector<std::string> palette;
   for(int i = 0; i< size; ++i){
 	
