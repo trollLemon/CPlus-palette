3cab84ff6eeb14f389ef8d7aabeddded7ea3aa94
Haydn <haydnt@zohomail.com>
2023-12-11 10:36:47 -0800

    basic implementation done: should_reduce, insert, reduce, etc...

--- src/octtree/oct_tree_node.cpp
+++ src/octtree/oct_tree_node.cpp
@@ -1,5 +1,6 @@
 #include "oct_tree_node.h"
 #include "color.h"
+#include <array>
 #include <cassert>
 #include <iostream>
 OctTreeNode::OctTreeNode(int level)
@@ -16,6 +17,11 @@ OctTreeNode::OctTreeNode(Color *col, int level)
   }
 }
 
+OctTreeNode::~OctTreeNode(){}
+
+
+OctTreeNode *OctTreeNode::at(int idx) { return children[idx]; };
+
 bool OctTreeNode::isLeaf() {
 
   for (int i = 0; i < CHILDREN; i++) {
@@ -26,39 +32,61 @@ bool OctTreeNode::isLeaf() {
   return true;
 }
 
+std::array<OctTreeNode *, CHILDREN> OctTreeNode::getChildren() {
+  return children;
+}
+
+void OctTreeNode::reduce(){
+
+	int ave_r =0;
+	int ave_g=0;
+	int ave_b =0;
+        int count =0;
+	for(OctTreeNode * child: children){
+		if(child==nullptr) continue;
+		Color * col = child->color;
+		ave_r+=col->Red();
+		ave_g+=col->Green();
+		ave_b+=col->Blue();
+		count++;
+		delete child;
+	}
+
+	color->setRGB(ave_r/count, ave_g/count, ave_b/count);
+
+}
+
 void OctTreeNode::add(Color *col) {
 
   count++;
   int red = col->Red() + color->Red();
   int green = col->Green() + color->Green();
   int blue = col->Blue() + color->Blue();
-
+  red/=count;
+  green/=count;
+  blue/=count;
   color->setRGB(red, green, blue);
 }
 
-
 void OctTreeNode::insert(Color *col) {
-    OctTreeNode *currentNode = this;
-    int depth = 0;
+  OctTreeNode *currentNode = this;
+  int depth = 0;
 
-    while (depth < LEVELS) {
-        int red = col->Red();
-        int green = col->Green();
-        int blue = col->Blue();
-        int idx = ((red & (1 << (7 - depth))) >> (7 - depth)) << 2 |
-                  ((green & (1 << (7 - depth))) >> (7 - depth)) << 1 |
-                  ((blue & (1 << (7 - depth))) >> (7 - depth));
+  while (depth < LEVELS) {
+    int red = col->Red();
+    int green = col->Green();
+    int blue = col->Blue();
+    int idx = ((red & (1 << (7 - depth))) >> (7 - depth)) << 2 |
+              ((green & (1 << (7 - depth))) >> (7 - depth)) << 1 |
+              ((blue & (1 << (7 - depth))) >> (7 - depth));
 
-	std::cout << idx << std::endl;
-        if (currentNode->children[idx] == nullptr) {
-            currentNode->children[idx] = new OctTreeNode(depth);
-        }
-
-        currentNode = currentNode->children[idx];
-        depth++;
+    if (currentNode->children[idx] == nullptr) {
+      currentNode->children[idx] = new OctTreeNode(depth);
     }
 
-    currentNode->add(col);
+    currentNode = currentNode->children[idx];
+    depth++;
+  }
+
+  currentNode->add(col);
 }
-
-
